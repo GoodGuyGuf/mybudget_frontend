e078793f48bbb4c20b97e36ca14c39e4d93647d5
Ethan <ethan_gustafson@outlook.com>
2020-03-10 13:26:11 -0400

    Testing seed budget 1's expenses on the DOM

--- src/index.js
+++ src/index.js
@@ -15,6 +15,10 @@ function oneBudget(){
 
 function returnToMe(fetched){
     let h1 = document.createElement("h1");
-    h1.textContent = `${fetched.data.attributes.title}: ${fetched.data.attributes.bank}`
-    document.body.appendChild(h1)
+    h1.textContent = `${fetched.data.attributes.title}: ${fetched.data.attributes.bank}`;
+    document.body.appendChild(h1);
+
+    let h3 = document.createElement("h3");
+    h3.textContent = `Expense 1: Name: ${fetched.data.attributes.expenses[0].name} - Cost: ${fetched.data.attributes.expenses[0].cost} - Due Date: ${fetched.data.attributes.expenses[0].date}`;
+    document.body.appendChild(h3);
 }
