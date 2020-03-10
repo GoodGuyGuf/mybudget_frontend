4f5eb796d21c56e0314be5e44ca6da5d6b811c22
Ethan <ethan_gustafson@outlook.com>
2020-03-10 15:41:32 -0400

    added index to budgets Controller/routes for testing, changed fetch on index.js to budgets index

--- src/index.js
+++ src/index.js
@@ -2,28 +2,19 @@ let budgetLink = document.getElementById("navBudgets");
 let numberOfBudgets = document.getElementById("navInfo");
 
 budgetLink.addEventListener("click", function(e){
-    oneBudget();
+    allBudgets();
 }, {once : true});
 
-function oneBudget(){
-    fetch("http://localhost:3000/budgets/1")
+function allBudgets(){
+    fetch("http://localhost:3000/budgets")
     .then(function(response){
         return response.json()
     }).then(function(json){
-        returnToMe(json)
+        json.data.forEach(function(budget){
+            let obj = {...budget.attributes, id: budget.id};
+            new Budget(obj);
+        })
     })
 }
 
-function returnToMe(fetched){
-    let h1 = document.createElement("h1");
-    h1.textContent = `Budget for: ${fetched.data.attributes.title} - Balance: $${fetched.data.attributes.bank}`;
-    document.body.appendChild(h1);
 
-    fetched.data.attributes.expenses.forEach(function(expense){
-        let h3 = document.createElement("h3");
-        h3.textContent = `Expense ${expense.id}: Name: ${expense.name} - Cost: ${expense.cost} - Due Date: ${expense.date}`;
-        document.body.appendChild(h3);
-    })
-
-    numberOfBudgets.innerText += ` ${fetched.data.attributes.expenses.length}`;
-}
