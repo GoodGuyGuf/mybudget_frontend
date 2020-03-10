858a0ac2e7b1b33292bcab2c8a575200c3072374
Ethan <ethan_gustafson@outlook.com>
2020-03-10 12:39:29 -0400

    test fetch to localhost:3000/budgets/1 and appended to DOM after Event Listener Click

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 let budgetLink = document.getElementById("navBudgets");
+
+budgetLink.addEventListener("click", function(e){
+    oneBudget();
+}, {once : true});
+
+function oneBudget(){
+    fetch("http://localhost:3000/budgets/1")
+    .then(function(response){
+        return response.json()
+    }).then(function(json){
+        returnToMe(json)
+    })
+}
+
+function returnToMe(fetched){
+    let h1 = document.createElement("h1");
+    h1.textContent = `${fetched.data.attributes.title}: ${fetched.data.attributes.bank}`
+    document.body.appendChild(h1)
+}
