b54b3775464b1a456984f0e37fa9b7c18ac28cba
Ethan <ethan_gustafson@outlook.com>
2020-03-10 16:05:42 -0400

    added simple budget form, it only loads when create is clicked.

---
+++ src/form.js
@@ -0,0 +1,7 @@
+let form = document.getElementById("form");
+let create = document.getElementById("navCreate")
+form.style.display ="none"
+
+create.addEventListener("click", function(){
+    form.style.display = ""
+})
