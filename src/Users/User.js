68676ad15c3c4dc480a911683d4490d20df3d327
Ethan <ethan_gustafson@outlook.com>
2020-03-15 13:07:58 -0400

    specific username now appends to DOM

--- src/Users/User.js
+++ src/Users/User.js
@@ -37,6 +37,9 @@ function loginFetch(userObject){
              } else {
                 let userObj = {id: json.data.id, username: json.data.attributes.username}
                 new User (userObj)
+                nav.style.display = ""
+                let userName = document.getElementById("navUser");
+                userName.innerText = `${userObj.username}`;
 
                 let loginForm = document.getElementById("LogIn")
                 loginForm.style.display = "none";
