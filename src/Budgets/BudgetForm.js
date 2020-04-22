8f225d3102cf52ac0184a81611b45656381ac87f
Ethan <ethan_gustafson@outlook.com>
2020-04-22 04:24:37 -0400

    Home now renders a budget form

--- src/Budgets/BudgetForm.js
+++ src/Budgets/BudgetForm.js
@@ -1,5 +1,22 @@
 class BudgetForm{
     constructor(){
+
+        this.div = document.createElement("div");
+        this.div.id = "budgetFormContainer";
+        document.body.appendChild(this.div);
+
+        this.form = document.createElement("form");
+        this.form.id = "budgetForm"
+        this.form.innerHTML = "<form action='http://localhost:3000/budgets' method='POST'>" +
+          "<h2>New Budget: </h2>" +
+          "<label>Title:</label>" +
+          "<input id='budgetTitle' type='text' name='budget[title]' placeholder='Title'>" +
+          "<label>Total:</label>" +
+          "<input id='budgetBank' type='number' name='budget[bank]' placeholder='Budget Total'>" +
+          "<button id='budgetSubmit' type='button'>Submit</button>" +
+        "</form>"
+
+        this.div.appendChild(this.form)
         
     }
 }
