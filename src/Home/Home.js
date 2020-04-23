e5ea3be87f7ef1967d849128aa89ea7fa5da7b67
Ethan <ethan_gustafson@outlook.com>
2020-04-23 01:31:07 -0400

    users can now logout

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -8,8 +8,23 @@ class Home {
         this.username.innerHTML = `Username: ${user}`
         document.body.appendChild(this.username)
 
+        this.logout = document.createElement('a');
+        this.logout.id = "logout";
+        this.logout.innerHTML = "<a href='Logout'>Log Out</a>"
+        document.body.appendChild(this.logout)
+
         this.budgetForm = new BudgetForm(user_id)
         fetchExpenses()
         fetchBudgets()
+        this.handleLogout()
+    }
+
+    handleLogout(){
+        this.logout.addEventListener("click", event => {
+            event.preventDefault();
+
+            document.body.innerHTML = "";
+            new App;
+        })
     }
 }
