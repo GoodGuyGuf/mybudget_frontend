035a0e818fde00f5715e74074508bf4e727fb568
Ethan <ethan_gustafson@outlook.com>
2020-04-23 00:08:30 -0400

    when a new expense is created it is rendered to DOM

--- src/Expenses/CreateExpense.js
+++ src/Expenses/CreateExpense.js
@@ -16,9 +16,8 @@ function createExpense(object){
         if (json.message === "Not Accepted"){
             new ExpenseError(object.budget_id)
         } else {
-            console.log(json)
-            // let expenseObj = {id: json.data.id, ...json.data.attributes, user_id: json.data.attributes.user_id}
-            // new Expense(expenseObj);
+            let expenseObj = {id: json.data.id, ...json.data.attributes}
+            new Expense(expenseObj);
         }
     }
     fetcher()
