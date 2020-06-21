44f5232392d061a907486704925bd93c6ea95e9a
Ethan <ethan_gustafson@outlook.com>
2020-06-21 01:58:15 -0400

    semantically refactored Signup.js + added readability

--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -1,35 +1,33 @@
 class Signup {
     constructor(){
 
-      this.div = document.createElement("div");
-      this.div.id="SignUp";
-      document.body.appendChild(this.div);
+      this.section = document.createElement("section");
+      this.section.id="SignUp";
+      this.section.innerHTML = `
+        <form id='signupForm' action='http://localhost:3000/users' method='POST'>
+          <h2>Sign up</h2>
+          <input id='username' type='text' name='user[username]' placeholder='Username'/><br/>
+          <input id='password' type='password' name='user[password]' placeholder='Password'/><br/>
+          <input type='submit' name='submit' value='Submit'/>
+        </form>
+        `;
 
-      this.form = document.createElement("form")
-      this.form.id = 'signupForm'
-      this.form.innerHTML = "<form action='http://localhost:3000/users' method='POST'>" +
-          "<h2>Sign up</h2>" +
-          "<input id='username' type='text' name='user[username]' placeholder='Username'/><br/>" +
-          "<input id='password' type='password' name='user[password]' placeholder='Password'/><br/>" +
-          "<input type='submit' name='submit' value='Submit'/>" +
-        "</form>"
+      document.body.appendChild(this.section);
 
-      this.loginLink = document.createElement("div")
-      this.loginLink.innerHTML = 
-      "<div id='loginDiv'>" +
-          "<h3>Already Have An Account?</h3>" +
-          "<button id='loginButton'>Log In</button>" +
-      "</div>"
-      
-      this.div.appendChild(this.form)
+      this.loginLinkContainer = document.createElement("section");
+      this.loginLinkContainer.id = 'loginDiv'
+      this.loginLinkContainer.innerHTML = `
+          <h3>Already Have An Account?</h3>
+          <button id='loginButton'>Log In</button>
+      </section>`;
 
-      document.body.appendChild(this.loginLink)
+      document.body.appendChild(this.loginLinkContainer)
       this.hideSignup()
       this.handleSignUp()
     }
 
     handleSignUp(){
-      this.form.addEventListener("submit", event => {
+      document.querySelector("#signupForm").addEventListener("submit", event => {
         event.preventDefault();
         let username = document.getElementById("username").value;
         let password = document.getElementById("password").value;
