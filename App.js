cb1f55f2b5faa991cc9c3a062acb42e856eae5e3
Ethan <ethan_gustafson@outlook.com>
2020-06-21 00:56:05 -0400

    added header to login page, moved script tags in index.html to head

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 class App {
     constructor(){
+        this.header = document.createElement("header");
+        document.body.appendChild(this.header);
+
         this.logo = document.createElement("h1");
         this.logo.id = "Logo";
-        this.logo.innerHTML = "<h1>myBudget</h1>";
-        document.body.appendChild(this.logo);
+        this.logo.innerHTML = "myBudget";
+        this.header.appendChild(this.logo);
         
         new Login;
     }
