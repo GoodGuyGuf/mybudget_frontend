62433e65b3b8f1b4b92901b5c5b72708a07bd94a
Ethan <ethan_gustafson@outlook.com>
2020-06-21 01:52:47 -0400

    semantically refactored ExpenseForm.js

--- src/Expenses/ExpenseForm.js
+++ src/Expenses/ExpenseForm.js
@@ -5,9 +5,8 @@ class ExpenseForm {
 
         this.budget = document.getElementById(`budget${this.budgetId}`)
 
-        this.form = document.createElement("form");
-        this.form.id = `Budget_${this.budgetId}ExpenseForm`;
-        this.form.innerHTML = `<form action="http://localhost:3000/expenses" method="POST">
+        this.formContainer = document.createElement("div");
+        this.formContainer.innerHTML = `<form id='Budget_${this.budgetId}ExpenseForm' action="http://localhost:3000/expenses" method="POST">
         <h3>New Expense: </h3>
         <label>Name: </label>
         <input id="name${this.budgetId}" type="text" name="expense[name]" placeholder="Name">
@@ -23,12 +22,12 @@ class ExpenseForm {
       <button id='submit' value='Submit' type='submit'>Submit</button>
     </form>`
 
-    this.budget.appendChild(this.form)
+    this.budget.appendChild(this.formContainer)
     this.submitForm()
     }
 
     submitForm(){
-        this.form.addEventListener("submit", event => {
+        document.querySelector(`#Budget_${this.budgetId}ExpenseForm`).addEventListener("submit", event => {
             event.preventDefault()
             let expenseName = document.getElementById(`name${this.budgetId}`).value
             let expenseCost = document.getElementById(`cost${this.budgetId}`).value
