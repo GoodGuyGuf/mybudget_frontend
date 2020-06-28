d794c56de58a99e09bab3cd81bedcecce2caa545
Ethan <ethan_gustafson@outlook.com>
2020-06-27 20:48:12 -0400

    added comments detailing how .append() would be used instead of createTextNode

--- App.js
+++ App.js
@@ -21,3 +21,20 @@ class App {
     }
 
 }
+
+/* *Using these methods provides functionality for older browsers.* There is a new method to use that would simplifiy 
+    the procedure above, and that is by using .append() - which will append any string to a specified parent node. 
+    HOWEVER, the .append() method does NOT work for oldr browsers. 
+*/
+
+/* If you were to write the code above by using .append() only, it would look like this: 
+
+    this.header = document.createElement("header");
+    this.logo = document.createElement("h1");
+    this.logo.setAttribute("id", "Logo"); 
+    this.logo.append("myBudget");
+
+    this.header.appendChild(this.logo);
+    document.body.appendChild(this.header);
+
+*/
