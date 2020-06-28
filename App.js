7e875fe477f176749609d706054463193881ab3f
Ethan <ethan_gustafson@outlook.com>
2020-06-27 20:53:48 -0400

    added polyfill information to App.js

--- App.js
+++ App.js
@@ -22,12 +22,17 @@ class App {
 
 }
 
-/* *Using these methods provides functionality for older browsers.* There is a new method to use that would simplifiy 
-    the procedure above, and that is by using .append() - which will append any string to a specified parent node. 
-    HOWEVER, the .append() method does NOT work for oldr browsers. 
+/* 
+
+*Using these methods provides functionality for older browsers.* There is a new method to use that would simplifiy 
+the procedure above, and that is by using .append() - which will append any string to a specified parent node. 
+HOWEVER, the .append() method does NOT work for older browsers. 
+
 */
 
-/* If you were to write the code above by using .append() only, it would look like this: 
+/* 
+
+If you were to write the code above by using .append() only, it would look like this: 
 
     this.header = document.createElement("header");
     this.logo = document.createElement("h1");
@@ -36,5 +41,22 @@ class App {
 
     this.header.appendChild(this.logo);
     document.body.appendChild(this.header);
+
+*/
+
+/*
+
+If you want to use .append(), you have to provide fallbacks to support older browsers. Read MDN on using a Polyfill for
+Internet Explorer 9 and below.
+
+A Polyfill is a piece of code (usually JavaScript on the Web) used to provide modern functionality on older browsers
+that do not natively support it. ... The polyfill uses non-standard features in a certain browser to give JavaScript 
+a standards-compliant way to access the feature.
+
+A Note on Polyfills:
+https://developer.mozilla.org/en-US/docs/Glossary/Polyfill
+
+Polyfill link for .append():
+https://developer.mozilla.org/en-US/docs/Web/API/ParentNode/append#Polyfill
 
 */
